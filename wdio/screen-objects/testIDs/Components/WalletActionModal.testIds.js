8bd107411d2f50fcd75d58f2a996ed4e96c29210
Davide-NXT <127792204+Davide-NXT@users.noreply.github.com>
2023-09-14 19:08:47 +0200

    Delete wdio directory

--- wdio/screen-objects/testIDs/Components/WalletActionModal.testIds.js
+++
@@ -1,3 +0,0 @@
-export const WALLET_SEND_ACTION_BUTTON = 'wallet-send-action';
-
-export const WALLET_RECEIVE_ACTION_BUTTON = 'wallet-receive-action';
--- wdio/screen-objects/testIDs/Screens/TokenOverviewScreen.testIds.js
+++
@@ -1,5 +0,0 @@
-export const TOKEN_ASSET_OVERVIEW = 'token-asset-overview';
-
-export const ASSET_BACK_BUTTON = 'asset-back-button';
-
-export const TOKEN_OVERVIEW_SEND_BUTTON = 'token-send-button';
